7b8602709e19ec5d8e13a4d8fce64c4c96666652
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-08-15 19:23:58 -0300

    Todo listo

---
+++ Struct de datos/Assets/Oushit.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oushit : MonoBehaviour
+{
+    [SerializeField]
+    private int speed;
+
+    private void Update()
+    {
+        speed++;
+        if (true)
+        {
+            
+        }
+        print(speed);
+    }
+}
