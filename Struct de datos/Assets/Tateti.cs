603c07541b8d61b3dc790851c96df5579d2c11d3
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-08-18 17:17:44 -0300

    Anashe

--- Struct de datos/Assets/Tateti.cs
+++ Struct de datos/Assets/Tateti.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,24 +12,26 @@ public class Tateti : MonoBehaviour
         set => _playerATurn = value;
     }
 
-    private const int Three=3;
+    private const int Columns = 3;
+    private const int Rows = 3;
 
-    private TextMeshProUGUI[,] _texts = new TextMeshProUGUI[Three,Three];
+    private TextMeshProUGUI[,] _texts = new TextMeshProUGUI[Rows, Columns];
 
-    private bool _playerATurn=false;
+    private bool _playerATurn = false;
+
     private void Awake()
     {
-        _playerATurn = Random.Range(0, 1) ==1 ? false : true;
-        
+        _playerATurn = Random.Range(0, 1) == 1 ? false : true;
+
         var buttons = GetComponentsInChildren<Button>();
 
         for (int i = 0; i < buttons.Length; i++)
         {
-            for (int j = 0; j < Three; j++)
+            for (int j = 0; j < Rows; j++)
             {
-                for (int k = 0; k < Three; k++)
+                for (int k = 0; k < Columns; k++)
                 {
-                    _texts[j,k]= buttons[i].GetComponentInChildren<TextMeshProUGUI>();
+                    _texts[j, k] = buttons[i].GetComponentInChildren<TextMeshProUGUI>();
                     _texts[j, k].text = "";
                 }
             }
@@ -37,18 +40,28 @@ public class Tateti : MonoBehaviour
 
     public void Check()
     {
-        for (int i = 0; i < Three; i++)
+        string last=String.Empty;
+        for (int i = 0; i < Rows; i++)
         {
-            for (int j = 0; j < Three; j++)
+            print($"I: {i}");
+            
+            for (int j = 0; j < Columns; j++)
             {
-                string last = _texts[i, j].text;
-                if (j>0)
+                print($"J: {j}");
+                
+                print(_texts[i, j].text);
+                if (j > 0)
                 {
-                
+                    // last = _texts[i, j - 1].text;
+                    // if ( != last)
+                    // {
+                    //     print("Es distinto");
+                    //     break;
+                    // }
                 }
-                
+              
             }
+        
         }
     }
-    
 }
