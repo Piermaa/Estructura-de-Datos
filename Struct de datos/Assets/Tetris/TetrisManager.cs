0d560bff39649c80cd44f17e817cf70a077371e0
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-09-01 16:44:08 -0300

    tetris

---
+++ Struct de datos/Assets/Tetris/TetrisManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TetrisManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] pieces;
+    [SerializeField] private Transform panel;
+    private Queue<GameObject> _piecesQueue = new Queue<GameObject>();
+    
+    public void DequeueAndEnqueue()
+    {
+        if (QueueIsEmpty())
+        {
+            DeQueuePiece();
+            EnqueuePiece();
+        }
+    }
+
+    private void DeQueuePiece()
+    {
+        if (QueueIsEmpty())
+        {
+            var obj = _piecesQueue.Dequeue();
+            print(obj.name);
+            Destroy(obj);
+        }
+    }
+
+    private void EnqueuePiece()
+    {
+        GameObject obj = Instantiate( pieces[Random.Range(0, pieces.Length)], this.panel);
+        _piecesQueue.Enqueue(obj);
+    }
+
+    public void GenerateQueue()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            EnqueuePiece();
+        }
+    }
+
+    private bool QueueIsEmpty()
+    {
+        return _piecesQueue.Count > 0;
+    }
+}
