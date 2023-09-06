e76b1372acb2301d5c4579fffaa3075f06c47a49
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-09-05 21:43:53 -0300

    Cola & Pila Dinamicas

--- Struct de datos/Assets/TDAs/IPilaTDA.cs
+++ Struct de datos/Assets/TDAs/IPilaTDA.cs
@@ -5,48 +5,10 @@ public interface IPilaTDA<T>
     // siempre que la pila est´e inicializada
     void Add(T x);
     // siempre que la pila est´e inicializada y no est´e vac´ıa
-    void Pop();
+    T Pop();
     // siempre que la pila est´e inicializada
     bool isEmpty();
     // siempre que la pila est´e inicializada y no est´e vac´ıa
     T Peek();
-}
-
-public class PilaTF<T> : IPilaTDA<T>
-{
-    T[] _array; // arreglo en donde se guarda la informacion
-    int _index; // variable entera en donde se guarda la cantidad de elementos que se tienen guardados
-
-    
-    public void Init(int length)
-    {
-        _array = new T[length];
-        _index = 0;
-    }
-    
-    public void Add(T x)
-    {
-        _array[_index] = x;
-        _index++;
-    }
-    public void Pop()
-    {
-        _index--;
-    }
-
-    public bool isEmpty()
-    {
-        return _index < 0;
-    }
-
-    public bool IsEmpty()
-    {
-        return (_index == 0);
-    }
-
-    public T Peek()
-    {
-        return (T)_array[_index - 1];
-    }
 }
 
---
+++ Struct de datos/Assets/TDAs/PilaTF.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PilaTF<T> : IPilaTDA<T>
+{
+    T[] _array; // arreglo en donde se guarda la informacion
+    int _index; // variable entera en donde se guarda la cantidad de elementos que se tienen guardados
+
+
+    public void Init(int length)
+    {
+        _array = new T[length];
+        _index = 0;
+    }
+
+    public void Add(T x)
+    {
+        _array[_index] = x;
+        _index++;
+    }
+
+    public T Pop()
+    {
+        _index--;
+        return _array[_index];
+    }
+
+    public bool isEmpty()
+    {
+        return _index < 0;
+    }
+
+    public bool IsEmpty()
+    {
+        return (_index == 0);
+    }
+
+    public T Peek()
+    {
+        return (T)_array[_index - 1];
+    }
+}
+
+
