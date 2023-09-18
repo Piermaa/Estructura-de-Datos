5b9a17fb562fe39bd53b7b836972259357bd829d
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-09-18 17:35:22 -0300

    FixedPilas

--- Struct de datos/Assets/TDAs/PilaTF.cs
+++ Struct de datos/Assets/TDAs/PilaTF.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,6 @@ public class PilaTF<T> : IPilaTDA<T>
 {
     T[] _array; // arreglo en donde se guarda la informacion
     int _index; // variable entera en donde se guarda la cantidad de elementos que se tienen guardados
-
 
     public void Init(int length)
     {
@@ -20,21 +20,21 @@ public class PilaTF<T> : IPilaTDA<T>
         _index++;
     }
 
+
     public T Pop()
     {
         _index--;
         return _array[_index];
     }
-
+    
     public bool isEmpty()
     {
         return _index < 0;
     }
     
-
     public T Peek()
     {
-        return (T)_array[_index - 1];
+        return _array[_index - 1];
     }
 }
 
