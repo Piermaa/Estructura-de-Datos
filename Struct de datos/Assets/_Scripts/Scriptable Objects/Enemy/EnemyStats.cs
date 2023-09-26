22452df507050bbde08f4522062dc8d7509407f5
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2023-09-26 14:44:12 -0300

    Cambio enemystats

--- Struct de datos/Assets/_Scripts/Scriptable Objects/Enemy/EnemyStats.cs
+++ Struct de datos/Assets/_Scripts/Scriptable Objects/Enemy/EnemyStats.cs
@@ -5,14 +5,12 @@ public class EnemyStats : ScriptableObject
 {
     [SerializeField] private EnemyStatsStruct _stats;
 
-    public int MaxLife => _stats.MaxLife; //No se usa, ahora mismo usa la del actor
     public int Damage => _stats.Damage;
     public int DifficultyLevel => _stats.DifficultyLevel;
 
     [System.Serializable]
     public struct EnemyStatsStruct
     {
-        public int MaxLife;
         public int Damage;
         public int DifficultyLevel;
     }
