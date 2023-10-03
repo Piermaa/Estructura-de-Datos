65961ea5ac1fcfacaf71cc46339bd27c8e6fe6d2
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2023-10-03 20:43:43 -0300

    Elimino el quicksort que no funca

--- Struct de datos/Assets/_Scripts/QuickSort.cs
+++ Struct de datos/Assets/_Scripts/QuickSort.cs
@@ -6,62 +6,6 @@ using System.Threading.Tasks;
 
 public class QuickSort<T> where T : IElementoConPrioridad, new()
 {
-    // public int Partition(T[] arr, int left, int right)
-    // {
-    //     int pivot;
-    //     int aux = (left + right) / 2; //tomo el valor central del vector
-    //     pivot = arr[aux].Priority;
-    //
-    //     // en este ciclo debo dejar todos los valores menores al pivot
-    //     // a la izquierda y los mayores a la derecha
-    //     while (true)
-    //     {
-    //         while (arr[left].Priority < pivot)
-    //         {
-    //             left++;
-    //         }
-    //
-    //         while (arr[right].Priority > pivot)
-    //         {
-    //             right--;
-    //         }
-    //
-    //         if (left < right)
-    //         {
-    //             T temp = arr[right];
-    //             arr[right] = arr[left];
-    //             arr[left] = temp;
-    //         }
-    //         else
-    //         {
-    //             // este es el valor que devuelvo como proxima posicion de
-    //             // la particion en el siguiente paso del algoritmo
-    //             return right;
-    //         }
-    //     }
-    // }
-    //
-    // public void quickSort(T[] arr, int left, int right)
-    // {
-    //     int pivot;
-    //     if (left < right)
-    //     {
-    //         pivot = Partition(arr, left, right);
-    //
-    //         if (pivot > 1)
-    //         {
-    //             // mitad del lado izquierdo del vector
-    //             quickSort(arr, left, pivot - 1);
-    //         }
-    //
-    //         if (pivot + 1 < right)
-    //         {
-    //             // mitad del lado derecho del vector
-    //             quickSort(arr, pivot + 1, right);
-    //         }
-    //     }
-    // }
-    
     public static void QuickSortMethod(T[] arr, int left, int right)
     {
         if (left < right)
