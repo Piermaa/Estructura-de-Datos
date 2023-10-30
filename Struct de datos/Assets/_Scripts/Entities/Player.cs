f7546a6a4f8e0be59bea94b0bb2d750fbd764982
Brian Czernikowski <90883778+BCzk@users.noreply.github.com>
2023-10-29 21:51:36 -0300

    tweak rbs, mov, navmesh, colliders

--- Struct de datos/Assets/_Scripts/Entities/Player.cs
+++ Struct de datos/Assets/_Scripts/Entities/Player.cs
@@ -10,21 +10,43 @@ public class Player : Actor, IMovable
 
     #region PRIVATE_VARIABLES
     [SerializeField] private float _moveSpeed;
+    private float xMovement = 0;
+    private float zMovement = 0;
+    private bool isMoving = false;
     #endregion
 
+    private void Update()
+    {
+        ListenForMoveInput();
+    }
     private void FixedUpdate()
     {
-        Move();
+        if (isMoving)
+        {
+            Move();
+        }
+        else HaltMovement();
+    }
+
+    private void ListenForMoveInput()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+        zMovement = Input.GetAxis("Vertical");
+
+        if (xMovement != 0 || zMovement != 0)
+            isMoving = true;
+        else isMoving = false;
     }
 
     #region IMOVABLE_METHODS
     public void Move()
     {
-        // Calculate inputs
-        float xMovement = Input.GetAxis("Horizontal");
-        float zMovement = Input.GetAxis("Vertical");
-
         actorRB.velocity = new Vector3(xMovement, 0, zMovement) * Time.deltaTime * _moveSpeed;
+    }
+    private void HaltMovement()
+    {
+        actorRB.velocity = Vector3.zero;
+        actorRB.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
     }
     #endregion
     [ContextMenu("/Kill")]
